d7d9f9c9bcf2b74a7147a0f4ee4da785e532e023
nikolaus-graf <nikolaus.a.graf@gmail.com>
2020-01-27 20:41:11 +0100

    moved logic from picture to fileSorter

--- src/main/java/org/graf/fotosort/model/Picture.java
+++ src/main/java/org/graf/fotosort/model/Picture.java
@@ -14,47 +14,28 @@ import org.slf4j.LoggerFactory;
 
 public class Picture {
 
-  protected Logger logger = LoggerFactory.getLogger(getClass());
-
   private final File path;
+  private final String hash;
 
   public Picture(File path) {
     this.path = path;
+    this.hash = buildHash(path);
+  }
+
+  private String buildHash(File path) {
+    try {
+      byte[] bytes = readAllBytes(get(path.toURI()));
+      return md5DigestAsHex(bytes);
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
   }
 
   public File getPath() {
     return path;
   }
 
-  public void copyTo(String name, File dir) {
-    try {
-      copyTo(name, dir, null);
-    } catch (IOException e) {
-      throw new UncheckedIOException(e);
-    }
-  }
-
-  private void copyTo(String name, File dir, Integer suffix) throws IOException {
-    String nameWithoutExtension = name.substring(0, name.lastIndexOf("."));
-    String extension = name.substring(name.lastIndexOf("."));
-
-    String finalName = nameWithoutExtension + (isNull(suffix) ? "" : "_" + suffix) + extension;
-    File destinationFile = new File(dir, finalName);
-
-    if (destinationFile.exists()) {
-      if (buildHash(path).equals(buildHash(destinationFile))) {
-        logger.info("file already exist with same hash: {}", finalName);
-        return;
-      }
-      copyTo(name, dir, (isNull(suffix) ? 1 : suffix + 1));
-    } else {
-      copyFile(path, destinationFile, true);
-      logger.info("copied file {} to {}", path.getAbsolutePath(), destinationFile.getAbsolutePath());
-    }
-  }
-
-  private String buildHash(File file) throws IOException {
-    byte[] bytes = readAllBytes(get(file.toURI()));
-    return md5DigestAsHex(bytes);
+  public String getHash() {
+    return hash;
   }
 }
--- src/main/java/org/graf/fotosort/service/FileSorter.java
+++ src/main/java/org/graf/fotosort/service/FileSorter.java
@@ -1,14 +1,24 @@
 package org.graf.fotosort.service;
 
+import static java.util.Objects.isNull;
+import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toUnmodifiableList;
+import static org.apache.commons.io.FileUtils.copyFile;
 import static org.apache.commons.io.FileUtils.listFiles;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import org.graf.fotosort.config.properties.FotoSortInfo;
 import org.graf.fotosort.config.properties.FotoSortProperties;
 import org.graf.fotosort.model.Picture;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,19 +27,58 @@ public class FileSorter {
   private final List<FileNameStrategy> fileNameStrategyList = new ArrayList<>();
   private final FotoSortProperties properties;
 
+  protected Logger logger = LoggerFactory.getLogger(getClass());
+
   public FileSorter(List<FileNameStrategy> fileNameStrategyList, FotoSortProperties properties) {
     this.properties = properties;
     this.fileNameStrategyList.addAll(fileNameStrategyList);
   }
 
   public void sort() {
+    Map<String, Picture> fotoHashMap = new HashMap<>();
     for (FotoSortInfo fotoSortInfo : properties.getFotoSortInfoList()) {
-      List<Picture> pictureList = listFiles(fotoSortInfo.getSourceDirectory(), null, true).stream()
-          .map(Picture::new)
-          .collect(toUnmodifiableList());
+      List<Picture> pictureList = buildPictureList(fotoSortInfo);
 
-      pictureList.forEach(picture -> picture.copyTo(renameTo(picture), fotoSortInfo.getTargetDirectory()));
+      pictureList.forEach(picture -> {
+        Optional<Picture> existingPictureOpt = ofNullable(fotoHashMap.get(picture.getHash()));
+        if (!existingPictureOpt.isPresent()) {
+          copyTo(picture, fotoSortInfo.getTargetDirectory());
+          fotoHashMap.put(picture.getHash(), picture);
+        } else {
+          Picture existingPicture = existingPictureOpt.get();
+          logger.info("file already exist with same hash: {} - {}", existingPicture.getPath().getAbsolutePath(), picture.getPath().getAbsolutePath());
+        }
+      });
     }
+  }
+
+  private void copyTo(Picture picture, File dir) {
+    try {
+      copyTo(picture, renameTo(picture), dir, null);
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+  }
+
+  private void copyTo(Picture picture, String name, File dir, Integer suffix) throws IOException {
+    String nameWithoutExtension = name.substring(0, name.lastIndexOf("."));
+    String extension = name.substring(name.lastIndexOf("."));
+
+    String finalName = nameWithoutExtension + (isNull(suffix) ? "" : "_" + suffix) + extension;
+    File destinationFile = new File(dir, finalName);
+
+    if (destinationFile.exists()) {
+      copyTo(picture, name, dir, (isNull(suffix) ? 1 : suffix + 1));
+    } else {
+      copyFile(picture.getPath(), destinationFile, true);
+      logger.info("copied file {} to {}", picture.getPath().getAbsolutePath(), destinationFile.getAbsolutePath());
+    }
+  }
+
+  private List<Picture> buildPictureList(FotoSortInfo fotoSortInfo) {
+    return listFiles(fotoSortInfo.getSourceDirectory(), null, true).stream()
+        .map(Picture::new)
+        .collect(toUnmodifiableList());
   }
 
   private String renameTo(Picture picture) {
